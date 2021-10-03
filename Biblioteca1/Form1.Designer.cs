911161cdf8f622589b988be30b2334ffff54e15c
lucaswalkergomes <85137927+lucaswalkergomes@users.noreply.github.com>
2021-10-03 19:22:14 -0300

    Add files via upload

---
+++ Biblioteca1/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿
+namespace Biblioteca1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgBiblioteca = new System.Windows.Forms.DataGridView();
+            this.colId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colNome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colCategoria = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colDescrição = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.colAno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nome = new System.Windows.Forms.Label();
+            this.Categoria = new System.Windows.Forms.Label();
+            this.Descrição = new System.Windows.Forms.Label();
+            this.Ano = new System.Windows.Forms.Label();
+            this.btLimpar = new System.Windows.Forms.Button();
+            this.btInserir = new System.Windows.Forms.Button();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.tbCategoria = new System.Windows.Forms.TextBox();
+            this.tbDescrição = new System.Windows.Forms.TextBox();
+            this.tbAno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbID = new System.Windows.Forms.TextBox();
+            this.btAlterar = new System.Windows.Forms.Button();
+            this.btDeletar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgBiblioteca)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgBiblioteca
+            // 
+            this.dgBiblioteca.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgBiblioteca.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.colId,
+            this.colNome,
+            this.colCategoria,
+            this.colDescrição,
+            this.colAno});
+            this.dgBiblioteca.Location = new System.Drawing.Point(36, 213);
+            this.dgBiblioteca.Name = "dgBiblioteca";
+            this.dgBiblioteca.Size = new System.Drawing.Size(733, 214);
+            this.dgBiblioteca.TabIndex = 0;
+            this.dgBiblioteca.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgBiblioteca_CellContentClick);
+            // 
+            // colId
+            // 
+            this.colId.HeaderText = "ID";
+            this.colId.Name = "colId";
+            this.colId.Width = 30;
+            // 
+            // colNome
+            // 
+            this.colNome.HeaderText = "Nome";
+            this.colNome.Name = "colNome";
+            this.colNome.Width = 150;
+            // 
+            // colCategoria
+            // 
+            this.colCategoria.HeaderText = "Categoria";
+            this.colCategoria.Name = "colCategoria";
+            this.colCategoria.Width = 150;
+            // 
+            // colDescrição
+            // 
+            this.colDescrição.HeaderText = "Descrição";
+            this.colDescrição.Name = "colDescrição";
+            this.colDescrição.Width = 300;
+            // 
+            // colAno
+            // 
+            this.colAno.HeaderText = "Ano";
+            this.colAno.Name = "colAno";
+            this.colAno.Width = 50;
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(121, 51);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 1;
+            this.Nome.Text = "Nome";
+            this.Nome.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Categoria
+            // 
+            this.Categoria.AutoSize = true;
+            this.Categoria.Location = new System.Drawing.Point(268, 51);
+            this.Categoria.Name = "Categoria";
+            this.Categoria.Size = new System.Drawing.Size(52, 13);
+            this.Categoria.TabIndex = 4;
+            this.Categoria.Text = "Categoria";
+            this.Categoria.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Descrição
+            // 
+            this.Descrição.AutoSize = true;
+            this.Descrição.Location = new System.Drawing.Point(432, 51);
+            this.Descrição.Name = "Descrição";
+            this.Descrição.Size = new System.Drawing.Size(55, 13);
+            this.Descrição.TabIndex = 6;
+            this.Descrição.Text = "Descrição";
+            this.Descrição.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Ano
+            // 
+            this.Ano.AutoSize = true;
+            this.Ano.Location = new System.Drawing.Point(599, 51);
+            this.Ano.Name = "Ano";
+            this.Ano.Size = new System.Drawing.Size(26, 13);
+            this.Ano.TabIndex = 8;
+            this.Ano.Text = "Ano";
+            // 
+            // btLimpar
+            // 
+            this.btLimpar.Location = new System.Drawing.Point(36, 108);
+            this.btLimpar.Name = "btLimpar";
+            this.btLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btLimpar.TabIndex = 9;
+            this.btLimpar.Text = "Limpar";
+            this.btLimpar.UseVisualStyleBackColor = true;
+            this.btLimpar.Click += new System.EventHandler(this.btLimpar_Click);
+            // 
+            // btInserir
+            // 
+            this.btInserir.Location = new System.Drawing.Point(119, 108);
+            this.btInserir.Name = "btInserir";
+            this.btInserir.Size = new System.Drawing.Size(75, 23);
+            this.btInserir.TabIndex = 10;
+            this.btInserir.Text = "Inserir";
+            this.btInserir.UseVisualStyleBackColor = true;
+            this.btInserir.Click += new System.EventHandler(this.btInserir_Click);
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(162, 48);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(100, 20);
+            this.tbNome.TabIndex = 11;
+            // 
+            // tbCategoria
+            // 
+            this.tbCategoria.Location = new System.Drawing.Point(326, 48);
+            this.tbCategoria.Name = "tbCategoria";
+            this.tbCategoria.Size = new System.Drawing.Size(100, 20);
+            this.tbCategoria.TabIndex = 12;
+            // 
+            // tbDescrição
+            // 
+            this.tbDescrição.Location = new System.Drawing.Point(493, 48);
+            this.tbDescrição.Name = "tbDescrição";
+            this.tbDescrição.Size = new System.Drawing.Size(100, 20);
+            this.tbDescrição.TabIndex = 13;
+            // 
+            // tbAno
+            // 
+            this.tbAno.Location = new System.Drawing.Point(631, 48);
+            this.tbAno.Name = "tbAno";
+            this.tbAno.Size = new System.Drawing.Size(100, 20);
+            this.tbAno.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // tbID
+            // 
+            this.tbID.Enabled = false;
+            this.tbID.Location = new System.Drawing.Point(57, 48);
+            this.tbID.Name = "tbID";
+            this.tbID.Size = new System.Drawing.Size(54, 20);
+            this.tbID.TabIndex = 16;
+            // 
+            // btAlterar
+            // 
+            this.btAlterar.Location = new System.Drawing.Point(200, 108);
+            this.btAlterar.Name = "btAlterar";
+            this.btAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btAlterar.TabIndex = 17;
+            this.btAlterar.Text = "Alterar";
+            this.btAlterar.UseVisualStyleBackColor = true;
+            this.btAlterar.Click += new System.EventHandler(this.btAlterar_Click);
+            // 
+            // btDeletar
+            // 
+            this.btDeletar.Location = new System.Drawing.Point(281, 108);
+            this.btDeletar.Name = "btDeletar";
+            this.btDeletar.Size = new System.Drawing.Size(75, 23);
+            this.btDeletar.TabIndex = 18;
+            this.btDeletar.Text = "Deletar";
+            this.btDeletar.UseVisualStyleBackColor = true;
+            this.btDeletar.Click += new System.EventHandler(this.btDeletar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btDeletar);
+            this.Controls.Add(this.btAlterar);
+            this.Controls.Add(this.tbID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbAno);
+            this.Controls.Add(this.tbDescrição);
+            this.Controls.Add(this.tbCategoria);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.btInserir);
+            this.Controls.Add(this.btLimpar);
+            this.Controls.Add(this.Ano);
+            this.Controls.Add(this.Descrição);
+            this.Controls.Add(this.Categoria);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.dgBiblioteca);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgBiblioteca)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgBiblioteca;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colNome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colCategoria;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colDescrição;
+        private System.Windows.Forms.DataGridViewTextBoxColumn colAno;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label Categoria;
+        private System.Windows.Forms.Label Descrição;
+        private System.Windows.Forms.Label Ano;
+        private System.Windows.Forms.Button btLimpar;
+        private System.Windows.Forms.Button btInserir;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.TextBox tbCategoria;
+        private System.Windows.Forms.TextBox tbDescrição;
+        private System.Windows.Forms.TextBox tbAno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbID;
+        private System.Windows.Forms.Button btAlterar;
+        private System.Windows.Forms.Button btDeletar;
+    }
+}
+
---
+++ Biblioteca1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private MySqlConnectionStringBuilder conexaoBanco()
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "biblioteca";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            conexaoBD.SslMode = 0;
+            return conexaoBD;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizaGrid();
+            dgBiblioteca.ColumnHeadersDefaultCellStyle.Font = new Font("Roboto", 10);
+        }
+
+        private void atualizaGrid()
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * FROM `biblioteca` WHERE 1";
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dgBiblioteca.Rows.Clear();
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dgBiblioteca.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//DESCRICAO
+                    row.Cells[3].Value = reader.GetString(3);//CATEGORIA
+                    row.Cells[4].Value = reader.GetString(4);//ANO
+                    dgBiblioteca.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            tbID.Clear();
+            tbAno.Clear();
+            tbNome.Clear();
+            tbCategoria.Clear();
+            tbDescrição.Clear();
+        }
+
+        private void btInserir_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+
+                comandoMySql.CommandText = "INSERT INTO biblioteca (nomeLivro,categoriaLivro,DescriçãoLivro,anoLivro) " +
+                    "VALUES('" + tbNome.Text + "', '" + tbCategoria.Text + "','" + tbDescrição.Text + "', " + Convert.ToInt16(tbAno.Text) + ")";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close();
+                MessageBox.Show("Inserido com sucesso");
+                atualizaGrid();
+                limparCampos();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btAlterar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "UPDATE biblioteca SET nomeLivro = '" + tbNome.Text + "', " +
+                    "descriçãoLivro = '" + tbDescrição.Text + "', " +
+                    "categoriaLivro = '" + tbCategoria.Text + "', " +
+                    "anoLivro = " + Convert.ToInt16(tbAno.Text) +
+                    " WHERE idLivro = " + tbID.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Atualizado com sucesso"); //Exibo mensagem de aviso
+                atualizaGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void btDeletar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = conexaoBanco();
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                // "DELETE FROM filme WHERE idFilme = "+ textBoxId.Text +""
+                comandoMySql.CommandText = "DELETE FROM biblioteca WHERE idLivro = " + tbID.Text + "";
+                //comandoMySql.CommandText = "UPDATE biblioteca SET ativoLivro = 0 WHERE idLivro = " + tbID.Text + "";
+
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Deletado com sucesso"); //Exibo mensagem de aviso
+                atualizaGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void dgBiblioteca_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgBiblioteca.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dgBiblioteca.CurrentRow.Selected = true;
+                //preenche os textbox com as células da linha selecionada
+                tbNome.Text = dgBiblioteca.Rows[e.RowIndex].Cells["colNome"].FormattedValue.ToString();
+                tbCategoria.Text = dgBiblioteca.Rows[e.RowIndex].Cells["colCategoria"].FormattedValue.ToString();
+                tbDescrição.Text = dgBiblioteca.Rows[e.RowIndex].Cells["colDescrição"].FormattedValue.ToString();
+                tbAno.Text = dgBiblioteca.Rows[e.RowIndex].Cells["colAno"].FormattedValue.ToString();
+                tbID.Text = dgBiblioteca.Rows[e.RowIndex].Cells["colID"].FormattedValue.ToString();
+            }
+        }
+    }
+    }
